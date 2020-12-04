4b6b16cf601e1ed67388eb43572f4e5dc4810fbb
Ctorum <vhcabralrezende@hotmail.com>
2020-12-04 12:42:20 -0300

    v0.1.0/BT-VH-Features

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('#modal');
+const button = document.querySelector('.firstButton');
+const span = document.querySelector('.close');
+
+button.addEventListener('click', function(){
+    modal.style.display = "flex";
+});
+
+span.addEventListener('click', function(){
+    modal.style.display = "none";
+});
